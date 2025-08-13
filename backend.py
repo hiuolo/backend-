0a2bd8f8d64d4e5be6d4760f37f7e1debb82c8b8
hiuolo <goldenseals186@yahoo.com>
2025-08-13 17:52:49 +0500

    fg

--- backend.py
+++ backend.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,   # не "*" при allow_credentials=True
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["*"],    
     allow_headers=["*"],
     expose_headers=["*"],
 )
